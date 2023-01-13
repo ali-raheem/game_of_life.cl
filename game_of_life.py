e7f6ae032816331a3c042f6e6bee8b8068813d50
Ali Raheem <ali.raheem@gmail.com>
2023-01-13 16:30:35 +0000

    Reduce peak memory usage

--- game_of_life.py
+++ game_of_life.py
@@ -1,12 +1,10 @@
 import pyopencl as cl
 import numpy
-
 import random
-
 from time import time
 
-WIDTH = 7680 
-HEIGHT = 4320
+WIDTH = 1920*4
+HEIGHT = 1080*4
 ITERATIONS = 200
 
 kernelsource = f"""
@@ -28,27 +26,18 @@ __kernel void iterate(
     c += !!currState[index(x-1, y, w, h)] + !!s + !!currState[index(x+1, y, w, h)];
     c += !!currState[index(x-1, y+1, w, h)] + !!currState[index(x, y+1, w, h)] + !!currState[index(x+1, y+1, w, h)];
     nextState[index(x, y, w, h)] = (uchar)(c==3) * (s + 1) + (uchar)(c == 4) * (s + !!s);
-//    if (c == 3) {{
-//        nextState[index(x, y, w, h)] = s + 1;
-//    }}else if (c == 4) {{
-//        nextState[index(x, y, w, h)] = s + !!s;
-//    }}else {{
-//        nextState[index(x, y, w, h)] = 0;
-//    }}
 }}
 """
 
 context = cl.create_some_context()
 queue = cl.CommandQueue(context)
-
 program = cl.Program(context, kernelsource).build()
 
 h_a = numpy.random.randint(2, size=WIDTH*HEIGHT, dtype=numpy.uint8)
 
-h_b = numpy.empty(WIDTH*HEIGHT, dtype=numpy.uint32)
-
 d_a = cl.Buffer(context, cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR, hostbuf=h_a)
-d_b = cl.Buffer(context, cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR, hostbuf=h_b)
+d_b = cl.Buffer(context, cl.mem_flags.HOST_NO_ACCESS, size=WIDTH*HEIGHT)
+
 rtime = time()
 
 iter = program.iterate
@@ -58,25 +47,18 @@ for _ in range(int(ITERATIONS/2)):
     iter(queue, (WIDTH, HEIGHT), None, d_a, d_b)
     iter(queue, (WIDTH, HEIGHT), None, d_b, d_a)
 
-queue.finish()
-rtime = time() - rtime
-
-print(f"Kernels completed in {rtime}s.")
-
-rtime = time()
+d_b.release()
 cl.enqueue_copy(queue, h_a, d_a)
 queue.finish()
+
+
 rtime = time() - rtime
-print(f"Unload from accelerator completed in {rtime}s.")
 
-cl.enqueue_copy(queue, h_a, d_b)
-queue.finish()
+print(f"Kernel completed and unlocked in {rtime}s.")
 
-#h_a = numpy.max(h_a) - h_a # Invert for black cells on white field
 h_a = h_a - numpy.min(h_a) # Normalize (and set max in PGM below)
 
 f = open("output.pgm", "w")
 f.write(f"P5\n{WIDTH} {HEIGHT}\n{numpy.max(h_a)}\n")
 h_a.tofile(f)
-
 f.close()
